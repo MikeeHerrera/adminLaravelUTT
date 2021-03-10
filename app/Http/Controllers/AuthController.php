008865331ba2ebecdffa15751828de0308c38176
MikeeHerrera <laloaburto62@gmail.com>
2021-03-09 21:22:55 -0600

    login, register , crud api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        if (Auth::attempt($request -> only('email','password'))) {
+
+            $autheticated_user  = Auth::user();
+            $user = User::find($autheticated_user -> id);
+            $token = $user-> createToken('admin')-> accessToken;
+
+            return [
+                'token' => $token
+            ];
+        }
+
+        return response ( [
+
+            'error' =>  'Invalid Credentials'
+        ], Response::HTTP_UNAUTHORIZED);
+
+    }
+
+    public function register(Request $request){
+
+        $user = User::create(
+
+            $request->only('first_name', 'last_name', 'email') +
+             ['password' => Hash::make($request -> input('password'))]
+        );
+
+        return response($user , Response::HTTP_CREATED );
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response ;
+
+class UserController extends Controller
+{
+    public function index(){
+      return  User::all();
+    }
+
+    public function show($id){
+
+      return User::find($id);
+    }
+
+       public function store(UserCreateRequest $request ){
+
+      $user = User::create(
+
+        $request->only('first_name', 'last_name', 'email') +
+        ['password' => Hash::make(12345678)
+
+        ]);
+
+      return response($user , Response::HTTP_CREATED);
+    }
+
+
+    public function update(UserUpdateRequest $request, $id){
+
+      $user  = User::find($id);
+      $user->update(
+
+        $request->only('first_name', 'last_name', 'email')
+
+
+      );
+
+      return response ($user,Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id){
+
+        User::destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
+      }
+
+}
+
+
+
+
+
+
+
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use App\Http\Requests\UserCreateRequest;
+// use App\Models\User;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Hash;
+// use Symfony\Component\HttpFoundation\Response ;
+
+// class UserController extends Controller
+// {
+//     public function index(){
+//       return  User::all();
+//     }
+
+//     public function show($id){
+
+//       return User::find($id);
+//     }
+
+//     public function store(UserCreateRequest $request ){
+
+//       $user = User::create(
+
+//         $request->only('first_name,last_name,email') +
+//         ['password' => Hash::make(12345678)]);
+
+//       return response($user , Response::HTTP_CREATED);
+//     }
+
+
+//     public function update(Request $request, $id){
+
+//       $user  = User::find($id);
+//       $user->update([
+//         'first_name' => $request->input('first_name'),
+//         'last_name' => $request->input('last_name'),
+//         'email' => $request ->input('email'),
+//         'password' => Hash::make($request ->input('password'))
+//       ]);
+
+//       return response ($user,Response::HTTP_ACCEPTED);
+//     }
+
+//     public function destroy($id){
+
+//       User::destroy($id);
+//       return response(null, Response::HTTP_NO_CONTENT);
+//     }
+// }
+
